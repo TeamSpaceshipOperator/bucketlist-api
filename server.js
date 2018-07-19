7b1f7ee3c333d356eb84bb8a406692acb356de4a
gofar09 <gordon.d.farrell@gmail.com>
2018-07-19 10:23:42 -0400

    Add search router for YELP API call -Curl script for test
    With @bentok2016

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const cors = require('cors')
 const exampleRoutes = require('./app/routes/example_routes')
 const userRoutes = require('./app/routes/user_routes')
 const restaurantRoutes = require('./app/routes/restaurant_routes')
+const searchRoutes = require('./app/routes/search_routes')
 
 // require database configuration logic
 // `db` will be the actual Mongo URI as a string
@@ -73,6 +74,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(exampleRoutes)
 app.use(userRoutes)
 app.use(restaurantRoutes)
+app.use(searchRoutes)
 
 // run API on designated port (4741 in this case)
 app.listen(port, () => {
